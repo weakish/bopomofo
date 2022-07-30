54a50087e366a14325bf0134130f805c0a87ac62
Jang Rush <weakish@gmail.com>
2022-07-30 13:26:11 +0000

    :new: two samples

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<style>
+  .group {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  :global(.popover + .popover) {
+    margin: 0 10px !important;
+  }
+
+  .content {
+    padding: 10px;
+    background: #fff;
+  }
+</style>
+
+<div class="group">
+  {#each bopomofo as e}
+  <Popover arrowColor="#fff" action="hover">
+    <button slot="target">{e.zhuyin}</button>
+    <div slot="content" class="content">{e.origin}</div>
+  </Popover>
+  {/each}
+
+</div>
+
+<script>
+  import Popover from 'svelte-popover'
+  const bopomofo = [
+	  { zhuyin: 'ㄅ', origin: '「包」之古字，取其聲'},
+	  { zhuyin: 'ㄆ', origin: '「扑」之古字，取其聲'},
+  ]
+</script>
